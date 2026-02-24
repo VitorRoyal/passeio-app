2f940d21a8a0a1b66715f8bfdbe79a7ff377725c
Vitor de Souza <130292943+VitorRoyal@users.noreply.github.com>
2026-02-24 14:50:08 -0300

    feat: arrumando title e subtitle das rotas

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutPropriedades } from './layoutpropriedades';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
 
+  propriedades: LayoutPropriedades = {
+    titulo: '',
+    subTitulo: ''
+  };
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.router.events
+    .subscribe(() => {
+      this.propriedades = this.obterPropriedadeLayout();
+    });
+  }
+
+  obterPropriedadeLayout(): LayoutPropriedades {
+    let rotaFilha = this.route.firstChild;
+
+    while (rotaFilha?.firstChild) {
+      rotaFilha = rotaFilha.firstChild;
+    }
+
+    return rotaFilha?.snapshot.data as LayoutPropriedades;
+  }
 }
